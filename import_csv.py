88d9650e2c47ff13504c8d6da02e952271fbdf6a
空想幻灵 <42439330+uitcis@users.noreply.github.com>
2022-02-27 11:38:45 +0800

    Add files via upload

---
+++ import_csv.py
@@ -0,0 +1,32 @@
+import bpy
+import csv
+
+# Each row read from the csv file is returned as a list of strings.
+
+def use_csv_bones_dictionary():
+	bones_dictionary = (__file__ + "bones_dictionary.csv").replace("import_csv.py" , "")
+	with open(bones_dictionary, newline='', encoding='utf-8') as csvfile:
+		CSVreader = csv.reader(csvfile, delimiter=',', skipinitialspace=True)
+		BONES_DICTIONARY = [tuple(x) for x in CSVreader]
+
+	# print('\n')
+	# print("BONES_DICTIONARY = ")
+	# for t in BONES_DICTIONARY:
+		# print(t , ",")
+
+	return BONES_DICTIONARY
+
+
+def use_csv_bones_fingers_dictionary():
+	finger_bones_dictionary = (__file__ + "bones_fingers_dictionary.csv").replace("import_csv.py" , "")
+	with open(finger_bones_dictionary, newline='', encoding='utf-8') as csvfile:
+		CSVreader = csv.reader(csvfile, delimiter=',', skipinitialspace=True)
+		FINGER_BONES_DICTIONARY = [tuple(x) for x in CSVreader]
+
+	# print('\n')
+	# print("FINGER_BONES_DICTIONARY = ")
+	# for t in FINGER_BONES_DICTIONARY:
+		# print(t , ",")
+
+	return FINGER_BONES_DICTIONARY
+
